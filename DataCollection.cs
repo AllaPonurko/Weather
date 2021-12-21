3cd60a3cd96a89c532c4d53f6aa8b42ba474f4fa
AllaPonurko <96493346+AllaPonurko@users.noreply.github.com>
2021-12-21 19:54:40 +0200

    Add files via upload

---
+++ DataCollection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Weather.WeatherData
+{/// <summary>
+/// сбор данных
+/// </summary>
+    public class DataCollection
+    {
+        private List<WeatherData> list1;
+
+        public List<WeatherData> list { get => list1; set => list1 = value; }
+        /// <summary>
+        /// добавление данных в лист с данными о погоде
+        /// </summary>
+        /// <param name="data"></param>
+        public void ListAdd(WeatherData data)
+        {
+            try 
+            {
+                if (list == null)
+                    list = new List<WeatherData>();
+                list.Add(data);
+            }
+            catch(Exception e)
+            {
+                WriteLine(e.Message);
+            }
+        }
+        /// <summary>
+        /// сериализация в xml формат листа с данными
+        /// </summary>
+        public void Save()
+        { XmlSerializer XmlFormat = new XmlSerializer(typeof(List<WeatherData>));
+            try
+            {
+                using (Stream fs = File.Create("weather.xml"))
+                {
+                    XmlFormat.Serialize(fs, list1);
+                }
+            }
+            catch(Exception e)
+            {
+                WriteLine(e.Message);
+            }
+        }
+        /// <summary>
+        /// десериализация из xml формата в лист с данными
+        /// </summary>
+        public List<WeatherData> Load()
+        {
+            List<WeatherData> datas = new List<WeatherData>();
+            XmlSerializer XmlFormat = new XmlSerializer(typeof(List<WeatherData>));
+            try
+            {
+                using (Stream fs = File.OpenRead("weather.xml"))
+                {
+                    datas = (List<WeatherData>)XmlFormat.Deserialize(fs);
+                }
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.Message);
+            }
+            return datas;
+        }
+        public void Print(List<WeatherData> datas)
+        {
+            foreach (var item in datas)
+            { WriteLine(item.ToString()); }
+        }
+    }
+}
---
+++ WeatherData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather.WeatherData
+{[Serializable]
+    public class WeatherData
+    {
+        public double Temperature { set; get; }
+        public int WindSpeed{ set; get; }
+        public int Pressure { set; get; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is WeatherData date &&
+                   Pressure == date.Pressure;
+        }
+        public DateTime dateTime = new DateTime();
+        /// <summary>
+        /// конструктор с тремя параметрами
+        /// </summary>
+        /// <param name="T">temperature</param>
+        /// <param name="WS">windspeed</param>
+        /// <param name="P">pressure</param>
+        public WeatherData(double T,int WS,int P)
+        {
+            Temperature = T;
+            WindSpeed = WS;
+            Pressure = P;
+            dateTime=DateTime.Now;
+        }
+        public WeatherData(DateTime time)
+        {
+            Random random = new Random();
+            if (time.Month == 1 || time.Month == 2 || time.Month == 12 && time.Hour >= 6.00 && time.Hour < 9.00)
+            {
+                Temperature = random.Next(0, 18) * (-1.33);
+                Pressure = random.Next(750, 765);
+                WindSpeed = random.Next(0, 12);
+            }
+            if (time.Month == 1 || time.Month == 2 || time.Month == 12 && time.Hour >= 9.00 && time.Hour < 17.00)
+            {
+                Temperature = random.Next(0, 8) * (-1.33);
+                Pressure = random.Next(750, 765);
+                WindSpeed = random.Next(4, 18);
+            }
+            if (time.Month == 1 || time.Month == 2 || time.Month == 12 && time.Hour >= 17.00 &&
+                time.Hour <= 24.00 )
+            {
+                Temperature = random.Next(0, 12) * (-1.33);
+                Pressure = random.Next(750, 765);
+                WindSpeed = random.Next(2,10);
+            }
+            if (time.Month == 1 || time.Month == 2 || time.Month == 12 && time.Hour >0.00 &&
+                time.Hour < 6.00)
+            {
+                Temperature = random.Next(0, 14) * (-1.33);
+                Pressure = random.Next(750, 765);
+                WindSpeed = random.Next(0,4);
+            }
+            dateTime = time;
+        }
+        public WeatherData() { }
+        public override string ToString()
+        {
+            return $"{dateTime}\n" +$"{Temperature}"+" C\n"+$"{Pressure }"+ " units of mercury column\n"+$"{WindSpeed}"+" m/c\n";
+        }
+        
+    }
+}
