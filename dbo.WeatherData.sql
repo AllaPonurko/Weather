2eb86c03b9a807c97ab88489332b145f2296f162
AllaPonurko <96493346+AllaPonurko@users.noreply.github.com>
2022-01-03 02:10:00 +0300

    Add files via upload

---
+++ dbo.WeatherData.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[WeatherData]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Temperature] FLOAT NULL, 
+    [WindSpeed] INT NULL, 
+    [Pressure] INT NULL, 
+    [DateTime] DATETIME NOT NULL
+)
